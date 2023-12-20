662be0a2102b36dd50a6cce87e54b69d384b8180
wonkyum-kim <dodo12120@naver.com>
2023-12-20 18:52:10 +0900

    feat: 다크 모드시에 화살표 배경색 변경

--- app/blog/components/pagination.tsx
+++ app/blog/components/pagination.tsx
@@ -102,10 +102,11 @@ function PaginationArrow({
   isDisabled?: boolean;
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center rounded-md border',
+    'flex h-10 w-10 items-center justify-center rounded-md border ',
     {
       'pointer-events-none text-gray-300': isDisabled,
       'hover:bg-gray-100': !isDisabled,
+      'dark:hover:bg-gray-500': !isDisabled,
       'mr-2 md:mr-4': direction === 'left',
       'ml-2 md:ml-4': direction === 'right',
     }
