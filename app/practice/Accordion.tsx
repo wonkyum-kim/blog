e246614b2bdd77fe4d2310be07bdfdf70d9cb286
wonkyum-kim <dodo12120@naver.com>
2024-02-21 17:25:28 +0900

    feat: add expand all button

--- app/practice/Accordion.tsx
+++ app/practice/Accordion.tsx
@@ -14,11 +14,13 @@ interface AccordionListProps {
   see: string;
   description: string;
   selected: string;
+  expandAll: boolean;
   handleClick: (name: string) => void;
 }
 
 export default function Accordion() {
   const [selected, setSelected] = useState('');
+  const [expandAll, setExpandAll] = useState(false);
 
   const handleClick = (name: string) => {
     setSelected((prev) => {
@@ -26,8 +28,16 @@ export default function Accordion() {
       return name;
     });
   };
+
+  const handleExpand = () => {
+    setExpandAll((prev) => !prev);
+  };
+
   return (
     <div className='w-full flex flex-col'>
+      <button onClick={handleExpand} className='hover:underline'>
+        {expandAll ? 'Collapse all' : 'Expand all'}
+      </button>
       {accordionItems.map((item) => {
         return (
           <AccordionList
@@ -35,6 +45,7 @@ export default function Accordion() {
             {...item}
             selected={selected}
             handleClick={handleClick}
+            expandAll={expandAll}
           />
         );
       })}
@@ -49,14 +60,15 @@ function AccordionList({
   source,
   description,
   selected,
+  expandAll,
   handleClick,
 }: AccordionListProps) {
-  console.log(selected);
+  const checked = selected === name || expandAll;
   return (
     <div
       className={clsx(
         'h-[52px] transition-all duration-300',
-        selected === name && 'md:h-auto min-[0px]:h-auto'
+        checked && 'md:h-auto min-[0px]:h-auto'
       )}
     >
       <div
@@ -71,11 +83,11 @@ function AccordionList({
         <ChevronDown
           className={clsx(
             'transition-all duration-500',
-            selected === name && 'rotate-180'
+            checked && 'rotate-180'
           )}
         />
       </div>
-      {selected === name && (
+      {checked && (
         <div className='min-h-[250px] w-full flex flex-col gap-2 items-center md:flex-row '>
           {img !== '' && (
             <Image
