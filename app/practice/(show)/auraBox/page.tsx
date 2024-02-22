359d2c04cbe9235b42cefdfa69bc3e4bc5af7397
wonkyum-kim <dodo12120@naver.com>
2024-02-22 11:25:07 +0900

    fix: escape character

--- app/practice/(show)/auraBox/page.tsx
+++ app/practice/(show)/auraBox/page.tsx
@@ -9,11 +9,11 @@ export default function AuraBox() {
         <div className={styles.box}>
           <h1>What is Lorem Ipsum?</h1>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries.
+            {`Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has survived
+            not only five centuries.`}
           </p>
         </div>
       </div>
