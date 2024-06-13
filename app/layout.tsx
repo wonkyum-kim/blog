40624fd9b4d69a30a090ba412f6ddec71bd3e788
wonkyum-kim <dodo12120@naver.com>
2024-06-13 18:36:39 +0900

    background decoration

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <div className='background-radish' />
+        <div className='background-blue' />
         {children}
         <Menu />
       </body>
