ec2f1ff10a14fe6a18b8cbcf0bb5528f00fd7f69
wonkyum-kim <dodo12120@naver.com>
2024-06-13 17:22:02 +0900

    fix TOC

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -7,7 +7,10 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // middleware 설정해서 서버 컴포넌트에서 현재 경로를 가져온다.
   const pathname = (headers().get('x-pathname') || '').split('/')
   const slug = pathname[pathname.length - 1]
-  const headings = getHeadingsBySlug(slug)
+  // id에서 ., /, @가 삭제되었기 때문에 진행한다.
+  const headings = getHeadingsBySlug(slug).map(({ heading, level }) => {
+    return { heading: heading.replace(/[.\/@]/g, ''), level }
+  })
 
   return (
     <div className={styles.main}>
--- components/toc.tsx
+++ components/toc.tsx
@@ -23,8 +23,6 @@ interface TOCProps {
 export function TOC({ headings }: TOCProps) {
   const [selected, setSelected] = useState('')
 
-  console.log(headings)
-
   useTOCEffect(setSelected)
 
   if (headings.length === 0) return
