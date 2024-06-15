579c1537f27c5bbf5e79e2e1ddaa7b12ce51900b
wonkyum-kim <dodo12120@naver.com>
2024-06-16 00:56:05 +0900

    docs: framer-motion-proxy

--- data/index.ts
+++ data/index.ts
@@ -4,6 +4,11 @@ export const frontmatter = {
     createdAt: '2024-06-14',
     tags: ['next', 'mdx'],
   },
+  ['framer-motion-proxy']: {
+    title: 'framer-motion에서 Proxy를 사용한 방법',
+    createdAt: '2024-06-04',
+    tags: ['framer-motion'],
+  },
   rollup: {
     title: 'Rollup을 사용하여 ESM과 CJS 모두 지원하는 라이브러리 만들기',
     createdAt: '2024-05-06',
