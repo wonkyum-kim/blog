d24d231a6e740380ab62a1c6df091547cc8fcac1
wonkyum-kim <dodo12120@naver.com>
2024-06-27 12:11:02 +0900

    feat: add copy success button

--- components/code-selector.tsx
+++ components/code-selector.tsx
@@ -11,6 +11,7 @@ interface CodeSelectorProps {
 
 export function CodeSelector({ names, desc }: CodeSelectorProps) {
   const [curr, setCurr] = useState(0)
+  const [copySuccess, setCopySuccess] = useState(false)
   const siblings = useRef<NodeListOf<Element>>()
 
   const setNameAttribute = () => {
@@ -43,11 +44,13 @@ export function CodeSelector({ names, desc }: CodeSelectorProps) {
 
   const handleCopy = () => {
     if (!siblings.current) return
+    setCopySuccess(true)
     const currentElement = siblings.current[curr] as HTMLPreElement
     navigator.clipboard.writeText(currentElement.textContent ?? ' ')
+    setTimeout(() => {
+      setCopySuccess(false)
+    }, 1500)
   }
-
-  // TODO: popover
 
   return (
     <nav className={styles.selector} data-for={desc}>
@@ -65,18 +68,31 @@ export function CodeSelector({ names, desc }: CodeSelectorProps) {
           </div>
         )
       })}
-      <svg
-        height='16'
-        viewBox='0 0 16 16'
-        version='1.1'
-        width='16'
-        fill='#fff'
-        className={styles['copy-button']}
-        onClick={handleCopy}
-      >
-        <path d='M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z'></path>
-        <path d='M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z'></path>
-      </svg>
+      {!copySuccess ? (
+        <svg
+          height='16'
+          viewBox='0 0 16 16'
+          version='1.1'
+          width='16'
+          fill='#fff'
+          className={styles['copy-button']}
+          onClick={handleCopy}
+        >
+          <path d='M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z'></path>
+          <path d='M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z'></path>
+        </svg>
+      ) : (
+        <svg
+          height='16'
+          viewBox='0 0 16 16'
+          version='1.1'
+          width='16'
+          className={styles['copy-button']}
+          fill='rgb(74 222 128)'
+        >
+          <path d='M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z'></path>
+        </svg>
+      )}
     </nav>
   )
 }
