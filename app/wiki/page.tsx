0c14eb94b579418636d98aeb14a440b718b0c462
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2024-07-22 16:15:57 +0900

    Carousel (#3)
    * style: table
    
    * feat: wiki 3d carousel

--- app/wiki/page.tsx
+++ app/wiki/page.tsx
@@ -3,7 +3,7 @@
 import styles from './page.module.css'
 import { wiki } from '@/data'
 import Link from 'next/link'
-import { UIEventHandler, useRef, useState } from 'react'
+import { useWikiCarouselStore } from '@/store/wiki-carousel'
 
 function replaceTitle(title: string) {
   return title.replace(/[\s@:]/g, (match) => (match === ' ' ? '-' : ''))
@@ -26,41 +26,39 @@ function getLinks(subject: keyof typeof wiki) {
 
 export default function WikiPage() {
   const subjects = Object.keys(wiki)
-  const [currIndex, setCurrIndex] = useState(0)
-  const prevTop = useRef(0)
 
-  const handleScroll: UIEventHandler<HTMLDivElement> = (event) => {
-    const scrollHeight = (event.target as HTMLDivElement).scrollHeight
-    const scrollTop = (event.target as HTMLDivElement).scrollTop
-    const oneCell = Math.floor(scrollHeight / subjects.length)
+  const currIndex = useWikiCarouselStore((state) => state.currIndex)
+  const inc = useWikiCarouselStore((state) => state.inc)
+  const dec = useWikiCarouselStore((state) => state.dec)
 
-    let nextIndex = 0
-
-    // down
-    if (prevTop.current < scrollTop) nextIndex = Math.floor(scrollTop / oneCell)
-    // up
-    else nextIndex = Math.floor((scrollTop + oneCell * 0.5) / oneCell)
-
-    prevTop.current = scrollTop
-
-    if (currIndex === nextIndex) return
-
-    setCurrIndex(nextIndex)
-  }
+  const yDeg = Math.floor(360 / subjects.length)
+  const tan = Math.tan(((Math.PI / 180) * yDeg) / 2)
 
   return (
     <>
-      <div className={styles['guide']}>스크롤하여 둘러보기</div>
-      <div className={styles['container']} onScroll={handleScroll}>
-        {subjects.map((sub) => {
-          return (
-            <div key={sub} className={styles['subject']}>
-              {sub}
-            </div>
-          )
-        })}
+      <div className={styles['guide']}>좌우를 클릭하여 이동</div>
+      <div className={styles['scene']}>
+        <div className={styles['carousel']}>
+          {subjects.map((sub, index) => {
+            return (
+              <div
+                key={sub}
+                className={styles['carousel__cell']}
+                //@ts-ignore
+                style={{ '--yDeg': `${yDeg * (index - currIndex)}deg`, '--tan': `${tan}` }}
+              >
+                {sub}
+                <div className={styles['move_left']} onClick={dec} />
+                <div className={styles['move_right']} onClick={inc} />
+              </div>
+            )
+          })}
+        </div>
       </div>
-      {getLinks(subjects[currIndex] as keyof typeof wiki)}
+
+      {getLinks(
+        subjects[(currIndex + subjects.length * 9999) % subjects.length] as keyof typeof wiki
+      )}
     </>
   )
 }
