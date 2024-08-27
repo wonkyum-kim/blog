4c1e115e90251201e7a022700fa217642c711df2
wonkyum-kim <dodo12120@naver.com>
2024-08-28 00:44:32 +0900

    refactor: menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -2,79 +2,65 @@
 
 import styles from './menu.module.css'
 
-import { useState, RefObject } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { viewTransition } from '@/lib/viewTransition'
 import { wiki } from '@/data'
-import { useClickAway } from '@uidotdev/usehooks'
 
 const links = ['Home', 'Posts', 'Wiki', 'GitHub']
 const urls = ['/', '/posts', '/wiki', 'https://github.com/wonkyum-kim']
 
 export function Menu() {
-  const ref = useClickAway(() => {
-    setSelected(null)
-    ref.current!.style.setProperty('background-color', 'transparent')
-  }) as RefObject<HTMLDivElement>
+  const [currMenu, setCurrMenu] = useState<null | string>(null)
   const router = useRouter()
-  const [selected, setSelected] = useState<string | null>(null)
+
+  const handleClickLink = (i: number) => {
+    viewTransition(() => {
+      setCurrMenu(links[i] as string)
+      if (links[i] === 'Wiki') return
+      router.push(urls[i] as string)
+    })
+  }
+
+  const handleHoverLink = (i: number) => {
+    setCurrMenu(links[i] as string)
+  }
+
+  const handleClickWiki = (item: string) => {
+    viewTransition(() => {
+      router.push(`/wiki/${item}`)
+      setCurrMenu(null)
+    })
+  }
+
+  const handleLeaveWiki = () => {
+    setCurrMenu(null)
+  }
 
   return (
-    <header className={styles['container']}>
-      <div className={styles['menu']} ref={ref}>
-        <div className={styles.list}>
-          {links.map((link, i) => {
-            return (
-              <li
-                key={link}
-                data-link={link}
-                style={{ color: selected === link ? 'red' : 'black' }}
-                onClick={() => {
-                  if (link === 'Wiki') {
-                    setSelected(link)
-                    return
-                  }
-                  viewTransition(() => router.push(urls[i] as string))
-                }}
-                onMouseOver={() => {
-                  setSelected(link)
-                  if (!ref.current) return
-
-                  ref.current.style.setProperty(
-                    'background-color',
-                    link === 'Wiki' ? 'white' : 'transparent'
-                  )
-                }}
-              >
-                {link}
-              </li>
-            )
-          })}
-        </div>
-
-        <div
-          className={styles['subject']}
-          style={{ display: selected === 'Wiki' ? 'grid' : 'none' }}
+    <div className={styles['container']}>
+      {links.map((link, i) => (
+        <li
+          key={link}
+          className={styles['list']}
+          data-curr={currMenu === link}
+          onClick={() => handleClickLink(i)}
+          onMouseOver={() => handleHoverLink(i)}
         >
-          {Object.keys(wiki).map((item) => {
-            return (
-              <div
-                key={item}
-                className={styles['item']}
-                onClick={() => {
-                  viewTransition(() => {
-                    setSelected(null)
-                    ref.current!.style.setProperty('background-color', 'transparent')
-                    router.push(`/wiki/${item}`)
-                  })
-                }}
-              >
-                {item}
-              </div>
-            )
-          })}
-        </div>
+          {link}
+        </li>
+      ))}
+
+      <div
+        className={[styles['wiki'], currMenu !== 'Wiki' ? styles['wiki-delete'] : ''].join(' ')}
+        onMouseLeave={handleLeaveWiki}
+      >
+        {Object.keys(wiki).map((item) => (
+          <div key={item} className={styles['wiki-item']} onClick={() => handleClickWiki(item)}>
+            {item}
+          </div>
+        ))}
       </div>
-    </header>
+    </div>
   )
 }
